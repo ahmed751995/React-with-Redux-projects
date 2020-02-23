5f82a75ddf45ea612e55f125b9099c08972379db
Ahmed Abdulrahman <ahmed751995@gmail.com>
2020-02-23 20:53:25 +0200

    marioplan && navbar created

---
+++ marioplan/src/components/Header.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand, NavItem, Nav, Collapse,
+	 NavbarToggler} from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+
+class Header extends Component {
+    constructor(props) {
+	super(props);
+	this.state = {
+	    isNavOpen: false  
+	};
+	this.toggleNav = this.toggleNav.bind(this);
+    }
+
+    toggleNav() {
+	this.setState({
+	    isNavOpen: !this.state.isNavOpen
+	});
+    }
+    
+    render() {
+	
+	return(
+	    <Navbar color="dark" dark expand="md">
+	      <div className="container">
+		<NavbarBrand className="mr-auto" href="/">Mario Plan</NavbarBrand>
+		<NavbarToggler onClick={this.toggleNav} />
+		<Collapse navbar isOpen={this.state.isNavOpen}>
+		  <Nav navbar className="ml-auto">
+		    <NavItem>
+		      <NavLink className="nav-link" to="/">Sign Up</NavLink>
+		    </NavItem>
+		    <NavItem>
+		      <NavLink className="nav-link" to="/">Log In</NavLink>
+		    </NavItem>
+		    <NavItem>
+		      <NavLink className="nav-link" to="/">New Project</NavLink>
+		    </NavItem>
+		    <NavItem>
+		      <NavLink className="nav-link" to="/">Log Out</NavLink>
+		    </NavItem>
+		    <NavItem>
+		      <NavLink className="nav-link" to="/">
+			<span className="fa fa-user fa-lg"></span>
+		      </NavLink>
+		    </NavItem>
+		  </Nav>
+		</Collapse>
+	      </div>
+	    </Navbar>
+	);
+    }
+}
+
+export default Header;
