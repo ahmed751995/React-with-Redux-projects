662d68322749ee987804aa37da243963fdd17700
Ahmed Abdulrahman <ahmed751995@gmail.com>
2020-04-07 23:09:33 +0200

    first stage

---
+++ markdown-previewer/src/components/main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Markdown from './markdown';
+
+class Main extends Component {
+    render() {
+	return(
+	    <div className="container">
+	      <Markdown />
+	    </div>
+	);
+    }
+}
+
+export default Main;
---
+++ markdown-previewer/src/components/markdown.js
@@ -0,0 +1,52 @@
+import React , { Component } from 'react';
+import marked from 'marked';
+import { Input, Form, FormGroup, Label} from 'reactstrap';
+
+class Markdown extends Component {
+    constructor(props) {
+	super(props);
+	this.state = {
+	    marked: '',
+	    text: ''
+	};
+	this.renderPreview = this.renderMarkdown.bind(this);
+	this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+	this.setState({
+	    text: event.target.value
+	});
+    }
+
+    renderMarkdown() {
+	let rawMarkup = marked(this.state.text, {sanitize: true});
+	return { __html: rawMarkup };
+    }
+    
+    render() {
+	return(
+	    <div className="row">
+	      <div className="col-12 col-md-6">
+		<Form>
+		  <FormGroup row>
+		    <div className="col">
+		      <Input type="textarea" name="text" id="markdown-text" rows={15}
+			     onChange={(e) => {
+				 this.handleChange(e);
+				 this.renderMarkdown();
+			}}/>
+		    </div>
+		  </FormGroup>
+		</Form>
+	      </div>
+	      <div className="col-12 col-md-6">
+		<div className="preview" dangerouslySetInnerHTML={this.renderMarkdown()}>
+		</div>
+	      </div>
+	    </div>
+	);
+    }
+}
+
+export default Markdown;
